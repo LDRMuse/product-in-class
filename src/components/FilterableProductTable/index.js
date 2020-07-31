e1bac7231c7ab36483827ec803a9b421f2cfba4f
Melissa <melissakheying@gmail.com>
2020-07-31 14:43:36 -0500

    refactir class for filterablePT

--- src/components/FilterableProductTable/index.js
+++ src/components/FilterableProductTable/index.js
@@ -1,10 +1,18 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 
-import {ProductTable} from './ProductTable'
-import {SearchBar} from './SearchBar'
+import { ProductTable } from './ProductTable'
+import { SearchBar } from './SearchBar'
 
-export const FilterableProductTable = () => <Fragment>
-  <p>FPT</p>
-  <ProductTable />
-  <SearchBar />
-</Fragment>
+
+export class Filter extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <p>FPT</p>
+        <ProductTable />
+        <SearchBar />
+      </Fragment>
+    )
+  }
+}
+
