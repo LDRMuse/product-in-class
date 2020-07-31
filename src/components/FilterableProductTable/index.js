9ae749f8939c5b8403583249a4ebae596de85294
Melissa <melissakheying@gmail.com>
2020-07-31 14:37:32 -0500

    Render starter components

---
+++ src/components/FilterableProductTable/index.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react'
+
+import {ProductTable} from './ProductTable'
+import {SearchBar} from './SearchBar'
+
+export const FilterableProductTable = () => <Fragment>
+  <p>FPT</p>
+  <ProductTable />
+  <SearchBar />
+</Fragment>
